643518db926f7fa4b1e501d3cfdc82cd9b714382
Colby Garland <colbygarland@gmail.com>
2022-10-11 22:51:46 -0600

    added og image

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -12,6 +12,7 @@ export const PageHead = ({ title = PAGE_TITLE, description = PAGE_DESCRIPTION }:
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="og:image" content="https://trick-or-treat-2022.netlify.app/social.png" />
       </Head>
     </>
   );
